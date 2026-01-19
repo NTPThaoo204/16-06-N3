8565767d4b067b171d2d716a11e17868d2a069de
thao <nguyennthao.204@gmail.com>
2026-01-19 09:44:25 +0700

    Delete addons/quan_ly_khach_hang directory

--- addons/quan_ly_khach_hang/models/lich_hen.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields
-class LichHen(models.Model):
-    _name = "lich_hen"
-    _description = "Lịch hẹn"
-    khach_hang_id = fields.Many2one('khach_hang', string="Khách hàng")
-    ngay = fields.Datetime(string="Ngày hẹn")
--- addons/quan_ly_khach_hang/models/tuong_tac.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields
-class TuongTac(models.Model):
-    _name = "tuong_tac"
-    _description = "Tương tác khách hàng"
-    khach_hang_id = fields.Many2one('khach_hang', string="Khách hàng")
-    noi_dung = fields.Text(string="Nội dung")
