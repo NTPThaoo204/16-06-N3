d85772598166f133f0ebc1e0798dedd384e8fb42
thao <nguyennthao.204@gmail.com>
2026-01-26 12:19:36 +0700

    Add files via upload

---
+++ addons/quan_ly_cong_viec/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from . import du_an
+from . import cong_viec
+from . import nguoi_tham_gia
+from . import bao_cao_tien_do
+from . import phan_bo_nguon_luc
---
+++ addons/quan_ly_khach_hang/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from . import khach_hang
+from . import tuong_tac
+from . import bao_gia
+from . import lich_hen
+from . import goi_dien
